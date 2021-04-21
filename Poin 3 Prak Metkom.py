4e7bef979805e64c282566a83cf12df3b5570ffb
DwikaMQ29 <82949259+DwikaMQ29@users.noreply.github.com>
2021-04-21 23:43:56 +0700

    Add files via upload

---
+++ Poin 3 Prak Metkom.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Dwika Miftahul Qohar    ||    20/459221/PA/19882
+
+# In[10]:
+
+
+print ("Program Menghitung Volume Balok")
+panjang = float(input("Mau panjangnya berapa = "))
+lebar = float(input("Mau lebarnya berapa   = "))
+tinggi = float(input("Mau tingginya berpa   = "))
+
+v = panjang*lebar*tinggi
+print ("==============Sehingga Didapatkan=============")
+print ("Volume Balok  = %0.2f" %v)
+
+
+# ## Sekian 
